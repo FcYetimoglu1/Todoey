fc7b1a9d3b5e8092531cb3a240ded70f41f9d874
FuatCan <fcyetimoglu@gmail.com>
2019-07-18 10:31:50 +0300

    Convert to MVC design pattern.

--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -10,30 +10,52 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    var itemArray=["Find Mike", "Buy Egges", "Destroy Demogorgon"]
+    var itemArray=[Item]()
     
     let defaults = UserDefaults.standard // uygulama kapatıldıgında eklenen itemleri kaydetme
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] { // uygulama kapatıldıgında eklenen itemleri kaydetme
-            itemArray=items
+        let newItem = Item()
+        newItem.title="Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title="Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title="Destroy Demogorgon"
+        itemArray.append(newItem3)
+        
+        
+        
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] { // uygulama kapatıldıgında eklenen itemleri kaydetme
+            itemArray = items
         }
     }
 
     //MARK - Tableview Datasource Methods
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count  //creates 3 cells
+        return itemArray.count  //creates  cells
         
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell=tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "ToDoItemCell")
         
-        cell.textLabel?.text=itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
         
+        cell.textLabel?.text=item.title
+        
+        //Ternary operator ==>
+        // value = condition ? valueIfTrue : valueIfFalse
+        
+        cell.accessoryType = item.done ? .checkmark : .none
+        
+       
         return cell  //reusable cell
     }
     //MARK: - TableView Delegate Methods
@@ -42,15 +64,11 @@ class ToDoListViewController: UITableViewController {
         //print(itemArray[indexPath.row])
         
   //Adding checkmark whne you clicked on it and removing again
-        if         tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-            
-                   tableView.cellForRow(at: indexPath)?.accessoryType = .none
-
-        } else {
-                   tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-
-        }
-
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+       
+       tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true) // to get rid of grey background color when it selected
     }
@@ -66,7 +84,10 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the add item button on our UIAlert
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item() //creating new item
+            newItem.title=textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "ToDoListArray") // uygulama kapatıldıgında eklenen itemleri kaydetme
             
---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by CAN on 16.07.2019.
+//  Copyright © 2019 CAN. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String=""
+    var done : Bool=false
+}
